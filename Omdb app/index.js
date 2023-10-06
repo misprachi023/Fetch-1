ccec9115506ab55762a89a5b8cb55cfec4bb6687
Prachi Mishra <143651644+misprachi023@users.noreply.github.com>
2023-10-06 13:48:35 +0530

    Update index.js

--- Omdb app/index.js
+++ Omdb app/index.js
@@ -1,44 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = '4f4b140a'; 
+    const searchInput = document.getElementById('searchInput');
+    const searchButton = document.getElementById('searchButton');
+    const movieData = document.getElementById('movieData');
 
-document.addEventListener('DOMContentLoaded' , () =>{
+    searchButton.addEventListener('click', () => {
+        const searchBar = searchInput.value.trim();
 
-    let apiKey = '4f4b140a'
-     let searchInput = document.getElementById('searchInput');
-     let searchButton = document.getElementById('searchButton');
-     let movieData = document.getElementById('movieData')
- 
-     searchButton.addEventListener('click' , () => {
- 
-         let searchBar = searchInput.value;
-         if(searchBar){
-             movieData.innerHTML = '';
- 
-             fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchBar}`)
-             .then(response => response.json() )
-             .then(data =>{
-                 if(data.Response === 'True'){
- 
-                     let movieTitle = document.createElement('h2');
-                     movieTitle.textContent = data.Title
- 
-                     let moviePoster = document.createElement('img');
-                     
-                     moviePoster.src = data.Poster
- 
-                     let movieDetails = document.createElement('h4');
-                     movieDetails.textContent = `Year : ${data.Year}, Rated : ${data.Rated}, Genre : ${data.Genre}`
- 
-                     movieData.appendChild(movieTitle)
-                     movieData.appendChild(moviePoster)
-                     movieData.appendChild(movieDetails)
-                 }else{
-                     movieData.textContent = 'Movie not Found!';
-                 }
-             })
-             .catch(error => {
-                 console.log("Error :" , error)
-                 movieData.textContent = 'An error occurred while fetching data.';
-             })
-         }
- 
-     })
- })
+        if (searchBar) {
+            movieData.innerHTML = '';
+
+            fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchBar}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.Response === 'True') {
+                        const movieTitle = document.createElement('h2');
+                        movieTitle.textContent = data.Title;
+
+                        const moviePoster = document.createElement('img');
+                        moviePoster.src = data.Poster || 'default-poster.jpg'; 
+
+                        const movieDetails = document.createElement('div');
+                        movieDetails.innerHTML = `
+                            <h4>Year: ${data.Year}</h4>
+                            <h4>Rated: ${data.Rated}</h4>
+                            <h4>Genre: ${data.Genre}</h4>
+                            <h4>Plot: ${data.Plot}</h4>
+                            <h4>Actors: ${data.Actors}</h4>
+                            <h4>Director: ${data.Director}</h4>
+                        `;
+
+                        movieData.appendChild(movieTitle);
+                        movieData.appendChild(moviePoster);
+                        movieData.appendChild(movieDetails);
+                    } else {
+                        movieData.textContent = 'Movie not found!';
+                    }
+                })
+                .catch(error => {
+                    console.log('Error:', error);
+                    movieData.textContent = 'An error occurred while fetching data.';
+                });
+        } else {
+            movieData.textContent = 'Please enter a movie title.';
+        }
+    });
+});
